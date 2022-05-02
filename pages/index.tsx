9ef0b60221f004867f5ca518a1b90b80541fb98c
David Hurley <dhurls99@gmail.com>
2022-05-02 06:26:50 -0400

    update design

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import Layout from '../components/Layout'
 const IndexPage = () => (
   <Layout title="Earn Arcade">
     <div className='w-11/12 mx-auto px-4 pt-10'>
-      <h1 className='text-xl md:text-7xl font-VT323'>Get Paid to Play Games Online</h1>
-      <p className='text-xl pt-2'>Imagine a world where you can get paid to play games like Wordle.</p>
-      <p className='text-xl'>That's the world we're building. <b>Launching May 8th.</b></p>
-      <p className='text-xl pt-8'>Follow <a className='text-blue-500 border-b-2 border-blue-500' href="https://twitter.com/davidhurley87" target="_blank">@davidhurley87</a> for the latest.</p>
+      <h1 className='text-6xl font-VT323'>Get Paid to Play Games Online</h1>
+      <p className='text-2xl pt-2'>Imagine a world where you can get paid to play games like Wordle.</p>
+      <p className='text-2xl'>That's the world we're building. <b>Launching May 8th.</b></p>
+      <p className='text-2xl pt-8'>Follow <a className='text-blue-500 border-b-2 border-blue-500' href="https://twitter.com/davidhurley87" target="_blank">@davidhurley87</a> for the latest.</p>
     </div>
   </Layout>
 )
