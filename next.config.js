7b84c361aa6ea327e2cf8952627d14b0138d7192
David Hurley <dhurls99@gmail.com>
2022-05-05 15:07:50 -0400

    bug fix

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ module.exports = {
   swcMinify: true,
   typescript: {
     // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
 }
