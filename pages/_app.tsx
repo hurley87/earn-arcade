cbbfce8a353a8cb74cb06cc08d577ade3488117b
David Hurley <dhurls99@gmail.com>
2022-05-10 09:22:56 -0400

    update fathom tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,23 +36,29 @@ const wagmiClient = createClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
+
   useEffect(() => {
-    if (process.env.NODE_ENV === "production") {
-      Fathom.load("DRPQZPZD", {
-        includedDomains: ["earnarcade.xyz"],
-      });
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load('DRPQZPZD', {
+      includedDomains: ['earnarcade.xyz'],
+    });
 
-      router.events.on("routeChangeComplete", function() {
-        Fathom.trackPageview();
-      });
-
-      return () => {
-        router.events.off("routeChangeComplete", function() {
-          Fathom.trackPageview();
-        });
-      };
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
     }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
   }, []);
+
   return (
     <WagmiProvider client={wagmiClient}>
       <RainbowKitProvider coolMode chains={chains} theme={midnightTheme()}>
