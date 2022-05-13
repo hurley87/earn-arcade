8f6be2783112d07a2da5452938f51b30f06947ff
David Hurley <dhurls99@gmail.com>
2022-05-13 16:47:04 -0400

    change game to wordle

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const IndexPage = () => {
       <p className='text-md md:text-lg pb-4'><a className='text-pink-500 border-b-2 border-pink-500 pb-1 mb-8' href="/leaderboard">See how much people are earning</a></p>
         <h1 className='text-4xl md:text-6xl font-bold max-w-xl mx-auto pt-2'>Get Paid to Play Games Online</h1>
         <p className='text-xl md:text-2xl pt-4'>Solve Puzzles and Earn $MATIC.</p>
-        <Link href="/wordle">
+        <Link href="/w3rdle">
           <button className="bg-pink-500 hover:bg-pink-400 text-white text-xl font-bold py-2 px-4 border-b-4 border-pink-700 hover:border-pink-500 rounded mt-6">
             Solve W3rdle and earn 1 $MATIC
           </button>
