20ccafc58dc6deefe80ac6e8128333ce85aef8cc
David Hurley <dhurls99@gmail.com>
2022-05-14 16:54:47 -0400

    update leaderboard to show conversion to USD

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const IndexPage = () => {
         <h1 className='text-4xl md:text-6xl font-bold max-w-xl mx-auto pt-2'>Get Paid to Play Games Online</h1>
         <p className='text-xl md:text-2xl pt-4'>Solve Puzzles and Earn $MATIC.</p>
         <Link href="/w3rdle">
-          <button className="bg-pink-500 hover:bg-pink-400 text-white text-xl font-bold py-2 px-4 border-b-4 border-pink-700 hover:border-pink-500 rounded mt-6">
+          <button className="bg-pink-500 hover:bg-pink-400 text-white text-xl font-bold py-2 px-4 border-b-4 border-pink-700 hover:border-pink-500 rounded mt-6 shadow-xl">
             Solve W3rdle and earn 1 $MATIC
           </button>
         </Link>
--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -2,16 +2,20 @@ import { GetStaticProps } from 'next'
 import Layout from '../components/Layout'
 import type { Transaction, Player } from "../interfaces"
 import Web3 from 'web3';
+import { useState } from 'react';
 
 interface Props {
     transactions: Transaction[]
+    price: number
 }
 
 function formatWallet(wallet: string) {
     return wallet.slice(0, 4) + "..." + wallet.slice(-4);
 }
   
-export default function Leaderboard({ transactions }: Props) {
+export default function Leaderboard({ transactions, price }: Props) {
+    const [denomination, setDenomination] = useState<string>("MATIC")
+
     const data: {[key:string]: number} = {}
     for (const i in transactions) {
         const transactionTo:string = transactions[i].to;
@@ -32,27 +36,37 @@ export default function Leaderboard({ transactions }: Props) {
         }
     })
 
+    function changeDenomination() {
+        denomination === 'MATIC' ? setDenomination('USD') : setDenomination('MATIC')
+    }
+
+    const total = players.reduce((a, b) => a + b.score, 0)
+
     return (
         <Layout title="Leaderboard | See where you rank">
             <div className="max-w-sm mx-auto w-full px-1 pb-36 pt-6 md:pt-0">
-                <div className='bg-pink-500 w-full flex justify-between text-white text-md rounded-md p-2 mb-4'>
-                    <p className='text-left font-bold'>Total Earnings</p>
-                    <p className='text-right font-bold'>{players.reduce((a, b) => a + b.score, 0)} $MATIC</p>
+                <div className='mx-4'>
+                    <div onClick={() => changeDenomination()} className='bg-pink-500 hover:bg-pink-400 text-white text-xl font-bold py-2 px-4 border-b-4 border-pink-700 hover:border-pink-500 rounded flex justify-between text-white text-md rounded-md p-2 md:mb-8 cursor-pointer shadow-xl'>
+                        <p className='text-left font-bold'>Total Earnings</p>
+                        <p className='text-right font-bold'>{denomination === 'MATIC' ? total : (total * price).toFixed(2)} ${denomination}</p>
+                    </div>
                 </div>
-                <div className='flex justify-between text-xl border-b-4 border-white pb-1 mb-4'>
+                <div className='flex justify-between text-xl border-b-4 border-white pb-1 mb-4 mx-4 mt-4'>
                     <p className='text-left font-bold'>Player</p>
                     <p className='text-right font-bold'>Earnings</p>
                 </div>
+                <div className='md:h-96 md:overflow-y-auto px-4'>
                 {
                     players.sort((a, b) => {
                         return b.score - a.score;
                       }).map((player, i) => (
                         <div key={i} className='flex justify-between pb-4 border-b-1 border-white'>
                             <p className='text-left'><span className='w-8 inline-block'>{i + 1}</span> {formatWallet(player.wallet)}</p>
-                            <p className='text-right'>{player.score}</p>
+                            <p className='text-right'>${denomination === 'MATIC' ? player.score : (player.score*price).toFixed(2)}</p>
                         </div>
                     ))
                 }
+                </div>
             </div>
         </Layout>
     )
@@ -87,9 +101,25 @@ export const getStaticProps: GetStaticProps = async ({  }) => {
         console.log(e)
     }
 
+    let price = 0.8
+    try {
+        const API_URL = `https://api.polygonscan.com/api?module=stats&action=maticprice&apikey=${POLY_API_KEY}`
+        const res = await fetch(API_URL, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "GET",
+        });
+        const response = await res.json();
+        price = parseFloat(response.result.maticusd)
+    } catch(e) {
+        console.log(e)
+    }
+
     return {
         props: {
             transactions,
+            price
         }
     }
 }
