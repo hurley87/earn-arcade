f755dc23a1e7f583434cb9197a1267b9d1533801
David Hurley <dhurls99@gmail.com>
2022-05-25 09:00:59 -0400

    remove comments and update packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,11 +38,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    // Initialize Fathom when the app loads
-    // Example: yourdomain.com
-    //  - Do not include https://
-    //  - This must be an exact match of your domain.
-    //  - If you're using www. for your domain, make sure you include that here.
     Fathom.load('JYEJRHHV', {
       includedDomains: ['earnarcade.xyz', 'www.earnarcade.xyz'],
     });
